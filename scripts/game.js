4565ee43d267e764cf330f6bd713e18712a233a0
Vijaylaxmi Biradar <vijaylaxmipb@gmail.com>
2024-07-06 17:48:47 +0000

    Add current game & play move button test to game.test.js

--- scripts/game.js
+++ scripts/game.js
@@ -1,8 +1,33 @@
 let game = {
-    score: 0,
     currentGame: [],
     playerMoves: [],
+    score: 0,
     choices: ["button1", "button2", "button3", "button4"],
+};
+
+function newGame() {
+    game.currentGame = [];
+    game.playerMoves = [];
+    game.score = 0;
+    showScore();
+    addTurn();
 }
 
-module.exports = { game };
+function addTurn() {
+    game.playerMoves = [];
+    game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
+
+}
+
+function showScore() {
+    document.getElementById("score").innetText = game.score;
+}
+
+function lightsOn(circ) {
+    document.getElementById(circ).classList.add("light");
+    setTimeout(function () {
+        document.getElementById(circ).classList.remove("light");
+    }, 400);
+
+}
+module.exports = { game, newGame, showScore, addTurn, lightsOn };
